37a2123622759388993347c0bc17d5d5a4c33922
lituancheng <lituancheng@baimaodai.com>
2018-09-26 19:09:53 +0800

    mail

---
+++ src/main/java/com/leon/ziru/mail/Mailer.java
@@ -0,0 +1,71 @@
+package com.leon.ziru.mail;
+
+import com.leon.ziru.log.ZRLogger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+/**
+ * 邮件发送
+ */
+@Component
+public class Mailer {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Value("${spring.mail.from}")
+    private String from;
+
+
+    /**
+     * 发送邮件
+     *
+     * @param subject 主题
+     * @param text    正文
+     */
+    @Async
+    public void sendSimpleMail(String subject, String text, String to) {
+        SimpleMailMessage message;
+        try {
+            message = new SimpleMailMessage();
+            message.setFrom(from);
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(text);
+            mailSender.send(message);
+        } catch (MailException e) {
+            ZRLogger.errorLog.error("sendMailError", e);
+        }
+    }
+
+    /**
+     * 发送邮件
+     *
+     * @param subject 主题
+     * @param text    正文
+     */
+    @Async
+    public void sendHtmlMail(String subject, String text, String to) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, false);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text, true);
+            mailSender.send(message);
+        } catch (MailException | MessagingException e) {
+            ZRLogger.errorLog.error("sendMailError", e);
+        }
+    }
+
+}
