6c1adc8408438aa127bf549b3e0137a8f7ba2799
lituancheng <lituancheng@baimaodai.com>
2018-10-16 09:45:40 +0800

    add some city

--- src/main/java/com/leon/ziru/model/consts/CityCode.java
+++ src/main/java/com/leon/ziru/model/consts/CityCode.java
@@ -2,7 +2,10 @@ package com.leon.ziru.model.consts;
 
 public enum  CityCode {
 
-    BJ("BJ", "110000"), SH("SH", "310000"), SZ("SZ", "440300"), GZ("GZ", "440100"); //北京，上海，深圳，广州
+    BJ("BJ", "110000"), SH("SH", "310000"), SZ("SZ", "440300"), GZ("GZ", "440100"), //北京，上海，深圳，广州
+    HZ("HZ", "330100"), NJ("NJ", "320100"), WH("WH", "420100"), CD("CD", "510100"), //杭州，南京，武汉，成都
+    TJ("TJ", "120000")  //天津
+    ;
 
     CityCode(String name, String code){
         this.name = name;
@@ -30,6 +33,16 @@ public enum  CityCode {
                 return SZ;
             case "GZ":
                 return GZ;
+            case "HZ":
+                return HZ;
+            case "NJ":
+                return NJ;
+            case "WH":
+                return WH;
+            case "CD":
+                return CD;
+            case "TJ":
+                return TJ;
             default:
                 return null;
         }
